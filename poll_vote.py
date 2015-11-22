a5f73228a10179ac39d4bc6f7e35ba091b63b247
vlad <vovkag@gmail.com>
2015-11-22 13:36:47 -0600

    Add some error handling v.0.2

--- poll_vote.py
+++ poll_vote.py
@@ -1,6 +1,6 @@
 __author__ = 'vlad'
 #######################################################################
-#   poll_vote.py v.0.1
+#   poll_vote.py v.0.2
 #
 #   This code designed to run on a Rasperry PI
 #   Please see https://github.com/liveplant/liveplant-bot/README.md for
@@ -17,8 +17,8 @@ import RPi.GPIO as GPIO
 poll_url = 'http://liveplant.herokuapp.com/current_action'
 poll_time = 30
 ## DO_NOT_FORGET Whenever a new action added to Rasperry Pi and liveplant-server you have to add the entry to the next array!!!
-actions = {'water' : {'GPIO' : 4, 'TIME_ON' : 15, 'TIME_STAMP' : 0 }, \
-           'light' : {'GPIO' : 3, 'TIME_ON' : 30, 'TIME_STAMP' : 0 }}
+actions = {'water' : {'GPIO' : 4, 'TIME_ON' : 15 }, \
+           'light' : {'GPIO' : 3, 'TIME_ON' : 30 }}
 sleep_time = poll_time
 
 
@@ -32,8 +32,8 @@ for action in actions :
 print('======> Start infinite loop to poll the liveplant server (get current_action JSON) from ',poll_url)
 # Poll the liveplant server for current action in an infinite loop
 
-# Temporarely limit by 9 polls, for that use count
-count = 9
+# Temporarely limit by 99 polls, for that use count
+count = 99
 # var to hold the last unixTimeStamp value (-1 is to make it initially exired)
 old_timeStamp = -1
 
@@ -41,27 +41,51 @@ while (count > 0):
     count = count - 1
 
     # Get JSON from the liveplant server
-    r = requests.get(poll_url)
-    ##print(r.status_code)
-    ##print(r.headers)
-    ##print(r.text[0:1000])
+    try:
+        r = requests.get(poll_url)
+    except requests.exceptions.RequestException as e:
 
-    # Parce JSON into object
-    j_obj = json.loads(r.text[0:1000])
+        # Something terribly wrong, cannot reach the server,
+        # just log logg the error and continue till next vote, hopefully somebody checking the log
+        print("!!!!!! ERROR Cannot reach the server at",poll_url,". Set action to 'nothing' till next poll")
+    else:
 
-    # Get values into variables
-    current_action = j_obj['action']
-    current_timeStamp = j_obj['unixTimestamp']
-    current_timeRemaning = j_obj['votingTimeRemaining']
+        # Parce JSON into object
+        try:
+            j_obj = json.loads(r.text[0:1000])
 
-    print('======> Received: ')
-    print('current_action         :',current_action)
-    print('current_timeStamp:',current_timeStamp)
-    print('current_timeRemaning:',current_timeRemaning)
+            # Get values into variables
+            current_action = j_obj['action']
+            current_timeStamp = j_obj['unixTimestamp']
+            current_timeRemaning = j_obj['votingTimeRemaining']
 
-    # A ZERO in votingTimeRemaining might indicate server is down... to be safe if ZERO change it to default
+            print('======> Received: ')
+            print('current_action      :',current_action)
+            print('current_timeStamp   :',current_timeStamp)
+            print('current_timeRemaning:',current_timeRemaning)
+        except:
+            print("!!!!!! ERROR No JSON received")
+            current_action = ''
+
+
+    # No action received means action is "nothing"
+    if not current_action or not (current_action in actions):
+        # Set values to defaults
+        current_action = 'nothing'
+        current_timeStamp = old_timeStamp
+        current_timeRemaning = poll_time
+
+        print("======> Faked values (cause did not receive 'action'): ")
+        print('current_action         :',current_action)
+        print('current_timeStamp      :',current_timeStamp)
+        print('current_timeRemaning   :',current_timeRemaning)
+
+
+    # A ZERO in votingTimeRemaining might indicate server is down... to be safe if ZERO
+    # change it to default prevent extremaly frequent poll
     if current_timeRemaning == 0 :
         current_timeRemaning = poll_time
+
 
     # Check if timestamp changed
     if old_timeStamp != current_timeStamp :
@@ -77,11 +101,11 @@ while (count > 0):
         # Check if any time left till next voting?
         if current_timeRemaning > actions[current_action]['TIME_ON'] :
             # Sleep one second longer after a new vote published
-            print("Sleep till second later then a new vote published")
+            print("Sleep till second later then a new vote published (for",current_timeRemaning - actions[current_action]['TIME_ON'] + 1,"seconds)")
             time.sleep(current_timeRemaning - actions[current_action]['TIME_ON'] + 1)
     else :
-        # We have got a new timestamp, act...
-        print("NO action needed, Sleep till second later then a new vote published")
+        # Do nothing just wait till one second after next votng and loop....
+        print("NO action needed, Sleep till second later then a new vote published (for",current_timeRemaning + 1,"seconds)")
         time.sleep(current_timeRemaning + 1)
 
 
